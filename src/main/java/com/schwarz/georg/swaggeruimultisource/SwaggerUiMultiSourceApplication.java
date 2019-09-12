432bc13e4c49c9f2e018c64ee8d9eda30cb838ac
Georg Schwarz <dev@georg-schwarz.com>
2019-09-12 17:51:39 +0200

    Add initialized Spring Boot project

---
+++ src/main/java/com/schwarz/georg/swaggeruimultisource/SwaggerUiMultiSourceApplication.java
@@ -0,0 +1,13 @@
+package com.schwarz.georg.swaggeruimultisource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerUiMultiSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerUiMultiSourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schwarz/georg/swaggeruimultisource/SwaggerUiMultiSourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.schwarz.georg.swaggeruimultisource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwaggerUiMultiSourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
